84991a76f765dde182a8de98cf63a6b31cc2914d
ilyes <armilyes@gmail.com>
2023-12-08 23:13:22 +0100

    adding auth flow without roles

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    # Local apps
+    path("auth/", include("userAuth.urls")),
     # Third party apps
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
--- userAuth/views.py
+++ userAuth/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import authentication_classes, permission_classes
 
 # Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        role =3
+        serializer.save(role=role)
+        #get user with hashed password
+        user = User.objects.get(email=request.data['email'])
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data})
+    return Response(serializer.errors, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def login(request):
+    email = request.data['email']
+    password = request.data['password']
+    try:
+        user = User.objects.get(email=email)
+        if user.check_password(password):
+            token = Token.objects.get(user=user)
+            return Response({'token': token.key, 'user': UserSerializer(user).data})
+        else:
+            return Response({'error': 'Wrong password'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response(status=status.HTTP_200_OK)
+
+#this one is for test purpose test_token
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def test_token(request):
+    return Response({'message': 'You are authenticated'}, status=status.HTTP_200_OK)
