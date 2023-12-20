8076f50232d0735659d9e71b70d4954e03e5e803
Ayyoub-Kasmi <ayyoubkasmidz@gmail.com>
2023-12-20 16:10:06 +0100

    tests edited

--- articles/tests.py
+++ articles/tests.py
@@ -4,23 +4,68 @@ from core.settings import DOCUMENTS_ROOT
 
 import os
 
+@pytest.fixture
+def drive_directory_url():
+    return "https://drive.google.com/drive/folders/10pzgHpHbTrOBRQ-m91MKGf6anYjgwQHG?usp=drive_link"
+
+@pytest.fixture
+def drive_file_url():
+    return "https://drive.google.com/file/d/1J5FxS2e1Iv_5enmqFsXwAZR_S2j5whI9/view?usp=drive_link"
+
+@pytest.fixture
+def file_name():
+    return "article.pdf"
+
+@pytest.fixture
+def folder_name():
+    return "articles_folder"
+
 # Create your tests here.
-def testSayHello(client):
-    url = reverse('articles_home')
-    response = client.get(url)
+def test_say_hello(client):
+    endpoint = reverse('articles_home')
+    
+    response = client.get(endpoint)
+
     assert response.status_code == 200
 
-def testFileUpload(client):
-    filename = "pdf1.pdf"
-    filepath = f"articles/tests_assets/{filename}"
+@pytest.mark.skip
+def test_one_input_file_upload(client, file_name):
+    endpoint = reverse('file_upload')
+
+    file_name = "pdf1.pdf"
+    filepath = f"articles/tests_assets/{file_name}"
 
     # check if the temp file already exists, and delete it if yes
-    if(os.path.exists(DOCUMENTS_ROOT + filename)):
-        os.remove(DOCUMENTS_ROOT + filename) 
+    if(os.path.exists(DOCUMENTS_ROOT + file_name)):
+        os.remove(DOCUMENTS_ROOT + file_name) 
 
-    url = reverse('file_upload')
     pdf = open(filepath, "rb")
 
-    response = client.post(url, {"articles": pdf}, format="multipart")
+    headers = {
+        # 'Content-Type': 'application/pdf'
+        'Content-Type': 'multipart/form-data; boundary=---------------------------974767299852498929531610575'
+    }
+
+    # response = client.post(endpoint, {"articles": pdf}, headers=headers, format="multipart")
+    response = client.post(endpoint, {"articles": pdf}, headers=headers)
+
+    assert response.status_code == 201
+
+def test_multiple_input_files_upload(client, file_name):
+    pass
+
+@pytest.mark.skip
+def test_one_google_drive_file_upload(client, file_name, drive_file_url):
+    endpoint = reverse('file_upload')
+
+    response = client.post(endpoint, {"url": drive_file_url, "name": file_name, "is-directoy": False})
+
+    assert response.status_code == 201
+
+@pytest.mark.skip
+def test_one_google_drive_folder_upload(client, folder_name, drive_directory_url):
+    endpoint = reverse('file_upload')
+
+    response = client.post(endpoint, {"url": drive_directory_url, "name": file_name, "is-directoy": True})
 
     assert response.status_code == 201
--- articles/views.py
+++ articles/views.py
@@ -1,43 +1,62 @@
 from django.http import HttpResponse
 from django.http.response import json
-from rest_framework import serializers
+import pytest
+from core.settings import DOCUMENTS_ROOT
+
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from django.core.files.storage import default_storage
 import gdown
 
 # Create your views here.
+@api_view(['GET'])
 def hello(request):
-    return HttpResponse("Welcome to articles!")
+    return Response("Welcome to articles!", status=status.HTTP_200_OK)
 
+@api_view(['POST'])
 def upload_file(request):  # use APIview or function based view or any view u want
-    # for single file
-    # file = request.FILES["article"]
-    # print(file.name)
-    # Do what ever you want with it
+    # Extracting data
 
-    # return upload_file_with_url(request)
-    # for multiple file
-    files = request.FILES.getlist('articles')
+    body = request.data
+    url = body.get('url')
+    is_directory = body.get('is-directory')
 
-    for file in files:
-        print(file.name)
-        # Do what ever you want with it
+    if url:
+        name = body.get('name')
+        if not name: return Response("Name should be specificed", status=status.HTTP_400_BAD_REQUEST)
 
-        default_storage.save(f"temp/files/{file.name}", file)
-        # try:
-        #     with open(f"temp/files/{file.name}", "wb") as buffer:
-        #         shutil.copyfileobj(.file, buffer)
-        # finally:
-        #     upload_file.file.close()
+        return upload_file_with_url(name, url, is_directory)
 
-    return HttpResponse(f'file(s) uploaded!', status=201)
+    else:
+        # the upload is through file input
 
-def upload_file_with_url(request):
-    print(request.body)
-    url = json.loads(request.body)['url']
-    print(url)
+        # for multiple file
+        files = request.FILES.getlist('articles')
+        print(files)
+        print(request.data)
+        if not len(files): return Response("No files provided. Enter either a file(s) or a URL", status=status.HTTP_400_BAD_REQUEST)
 
-    output = 'temp/files/pdf1.pdf'
-    gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+        for file in files:
+            print(file.name)
+            # Do what ever you want with it
 
-    return HttpResponse(f'file(s) uploaded!')
+            default_storage.save(DOCUMENTS_ROOT+file.name, file)
+
+        return Response(f'file(s) uploaded!', status=status.HTTP_200_OK)
+
+def upload_file_with_url(name, url, is_directory=False):
+    output = DOCUMENTS_ROOT + name
+
+    if is_directory:
+        # example URL: url = "https://drive.google.com/drive/folders/15uNXeRBIhVvZJIhL4yTw4IsStMhUaaxl"
+        gdown.download_folder(url, quiet=True, output=output, use_cookies=False)
+
+        return Response(f'file(s) uploaded!', status=status.HTTP_201_CREATED)
+
+    else:
+        # example URL: url = "https://drive.google.com/file/d/0B9P1L--7Wd2vNm9zMTJWOGxobkU/view?usp=sharing"
+        gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+
+        return Response(f'file uploaded!', status=status.HTTP_201_CREATED)
