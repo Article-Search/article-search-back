316b13b414f2562b97417e9639b1aec70e665d78
ilyes <armilyes@gmail.com>
2024-02-03 11:17:07 +0100

    adjusting selenium test

--- core/integration_test.py
+++ core/integration_test.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
 
 
 def test_login():
@@ -23,12 +28,15 @@ def test_login():
         password_field.send_keys("testpassword")
 
         # Submit the form
-        password_field.submit()
+        submit_button = driver.find_element(by=By.ID, value="submit")
+        submit_button.click()
 
         # Wait for the page to load and check if the user is signed in
         # This will depend on your application, for example you might check if a logout button is present
         # Here we just check if the title of the page has changed
-        assert "Logged In" in driver.title
+        # assert "Logged In" in driver.title
+        #instead check if the user is redirected to http://localhost:3000/search
+        WebDriverWait(driver, 10).until(EC.url_to_be("http://localhost:3000/search"))
     finally:
         # Close the browser
         driver.quit()
