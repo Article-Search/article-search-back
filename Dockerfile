1e9f018790d06ed4cf8c910da31ea5d367f99e0b
Ayyoub-Kasmi <ayyoubkasmidz@gmail.com>
2024-02-05 22:12:44 +0100

    adjustements added

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Use the official Python base image
+FROM python:3.10.12
+
+# Set the working directory
+WORKDIR /
+
+# Install dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the application code
+COPY . .
+
+# Expose the port the app will run on
+EXPOSE 8000
--- search/urls.py
+++ search/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 GET requests for the search app:
 
 1. **Searching for articles**:
-    - URL: http://127.0.0.1:8000/article-search/{query}/
+    - URL: http://127.0.0.1:8000/articles/article-search/{query}/
     - Description: Returns a list of articles that match the search term. The search is performed on the fields "title", "keywords", "content", "authors.first_name", "authors.last_name", and "institutions.name". The results are paginated and ordered by the `publish_date` field in descending order.
     -It also supports fuzziness if someone makes typos
     -Example: http://127.0.0.1:8000/article-search/Django/ To search for articles that contain the word "django"
